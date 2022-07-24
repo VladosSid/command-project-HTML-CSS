73cea6af33b64d5dcb7212628e3ae1e41281ae0a
VladosSid <104505584+VladosSid@users.noreply.github.com>
2022-07-24 20:00:13 +0000

    Deploying to gh-pages from @ VladosSid/command-project-HTML-CSS@40d761eebb8f27177ee0305b590653d9e5ac0172 🚀

--- index.b0696d42.js
+++
@@ -1,2 +0,0 @@
-!function(){new Splide(".splide",{type:"loop",perPage:1,arrows:!1,wheel:!0,speed:250}).mount();!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}()}();
-//# sourceMappingURL=index.b0696d42.js.map
